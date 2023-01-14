516cc440ee6f6b68b0129468f7e8aa751fffa579
СвященникИзАда <107143848+MikluhaRus@users.noreply.github.com>
2023-01-15 00:43:48 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Это моя семья");
+        System.out.println("Мама - Ольга");
+        System.out.println("Отец - Олег");
+        System.out.println("Я - Михаил");
+        System.out.println("Сестра - Регина");
+
+    }
+}
